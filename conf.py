c86bbf632460ccf16673fc2408a23002646ba8ca
aichaa28 <aicha.ettriki28@gmail.com>
2025-02-07 17:17:34 +0100

    sphinx

---
+++ conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Projet_final_data_viz'
+copyright = '2025, Aicha Ettriki and Youssef Abdelhedi'
+author = 'Aicha Ettriki and Youssef Abdelhedi'
+release = '07/02/2025'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # Génère des docstrings automatiquement
+    'sphinx.ext.napoleon',  # Pour les docstrings au format Google
+    'sphinx.ext.viewcode',  # Ajoute des liens vers le code source
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'eng'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,28 @@
+.. Final Project Data Visualization documentation master file, created by
+   sphinx-quickstart on Wed Feb  7 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Bienvenue sur la documentation de Final Project Data Visualization !
+=================================================================
+
+Introduction
+------------
+Ce projet vise à visualiser et analyser des données dans le but de faciliter la prise de décisions basées sur ces informations. L'objectif est de générer des visualisations intuitives à partir de différentes sources de données.
+
+Table des matières
+-------------------
+.. toctree::
+   :maxdepth: 2
+   :caption: Contenu:
+
+   installation
+   utilisation
+   references
+
+Installation
+------------
+Pour installer le projet, suivez ces étapes :
+1. Clonez le dépôt :
+   ```bash
+   git clone https://github.com/ton-utilisateur/final_project_data_viz.git
